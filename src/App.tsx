237db10dddf0e604cf561f9fb809521237a62826
Polina Skoraya <skorayapolina@gmail.com>
2020-01-27 17:54:54 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import axios from 'axios'
+import {observer} from "mobx-react";
+import {observable} from "mobx"
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+@observer
+class App extends Component {
+
+    @observable info: any = 'smth';
+
+    componentDidMount(): void {
+        axios.get('http://localhost:9000/testAPI')
+            .then( (response) => {
+                this.info = response.data;
+                console.log(this.info);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Edit <code>src/App.tsx</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+                <p>{this.info}</p>
+            </div>
+        );
+    }
 }
 
 export default App;
